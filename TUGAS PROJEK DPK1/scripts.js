3c46c84c8cc7e666bca2ec44a3222e18af6d1d85
DZKR1 <148258270+d2ikr1abadi@users.noreply.github.com>
2024-06-13 06:30:01 +0700

    Add files via upload

---
+++ TUGAS PROJEK DPK1/scripts.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+    var navbarLinks = document.getElementById("navbar-links");
+    if (navbarLinks.style.display === "block") {
+        navbarLinks.style.display = "none";
+    } else {
+        navbarLinks.style.display = "block";
+    }
+}
+
+
+
